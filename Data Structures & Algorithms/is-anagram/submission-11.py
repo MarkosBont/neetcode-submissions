dc79a8afe439cd9d4449e4a773d26cc6ecc1cb70
Markos Bontozoglou <72648745+MarkosBont@users.noreply.github.com>
2026-08-10 15:29:54 +0200

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hmap = {}
+        for char in s:
+            hmap[char] = hmap.get(char, 0) + 1
+        
+        for char in t:
+            if char not in hmap:
+                return False
+            
+            hmap[char] -= 1
+            if hmap[char] == 0:
+                del hmap[char]
+        
+        
+        return len(hmap) == 0
+        
