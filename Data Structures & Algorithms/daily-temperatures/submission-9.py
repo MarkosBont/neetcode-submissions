ac97a05917534979729f38e67c1e8d441ad38704
Markos Bontozoglou <72648745+MarkosBont@users.noreply.github.com>
2026-08-12 12:31:24 +0200

    Add: daily-temperatures - submission-9

---
+++ Data Structures & Algorithms/daily-temperatures/submission-9.py
@@ -0,0 +1,17 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        result = [0] * len(temperatures)
+
+        for i, temp in enumerate(temperatures):
+            while stack and temp > stack[-1][1]:
+                recent = stack.pop()
+                days = i - recent[0]
+                result[recent[0]] = days
+            
+            stack.append((i, temp))
+        
+        return result
+
+
+        
