0abec6c013ee0eeff32e343828b4efc37e9f6805
Markos Bontozoglou <72648745+MarkosBont@users.noreply.github.com>
2026-09-13 18:02:01 -0400

    Add: longest-repeating-substring-with-replacement - submission-18

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-18.py
@@ -0,0 +1,19 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0
+        longest = 0
+        hmap = {}
+
+        for r in range(len(s)):
+            hmap[s[r]] = hmap.get(s[r], 0) + 1
+
+            curr_length = r - l + 1
+            if curr_length - max(hmap.values()) <= k:
+                longest = max(longest, curr_length)
+            
+            else:
+                hmap[s[l]] -= 1
+                l += 1
+        
+        return longest
+        
